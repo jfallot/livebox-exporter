ae9cea3ccb73d057abe496a8a671606ef341a905
jfallot <jfallot@gmail.com>
2026-03-26 23:48:37 +0100

    Rollback changes done for https://github.com/Tomy2e/livebox-exporter/issues/26

--- main.go
+++ main.go
@@ -217,10 +217,6 @@ func main() {
 		writeHeaderVec,
 	)
 
-	// Unregister Go and Process collectors as they are not primarly needed
-	registry.Unregister(collectors.NewGoCollector())
-	registry.Unregister(collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}))
-
 	go func() {
 		for {
 			if err := pollers.Poll(ctx); err != nil {
@@ -240,13 +236,6 @@ func main() {
 			registry, promhttp.HandlerFor(registry, promhttp.HandlerOpts{}),
 		)),
 	)
-
-	// Exporter with gometrics only
-	promReg := prometheus.NewRegistry()
-	promReg.Register(collectors.NewGoCollector())
-	promReg.Register(collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}))
-	http.Handle("/gometrics", promhttp.HandlerFor(promReg, promhttp.HandlerOpts{}))
-
 	http.HandleFunc("/{$}", func(w http.ResponseWriter, _ *http.Request) {
 		_, _ = w.Write([]byte(indexPage))
 	})
